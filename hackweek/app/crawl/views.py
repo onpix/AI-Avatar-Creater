fe2e4bcac6c34f115198a186269283563f3b3d96
1@1.com <1@1.com>
2018-08-06 14:06:18 -0400

    fix(crawl): not impossible

--- hackweek/app/crawl/views.py
+++ hackweek/app/crawl/views.py
@@ -5,12 +5,8 @@ from . import crawl
 from flask import render_template,request    
 USER_NUM = 0
 # OUT_PATH = '/run/media/why/DATA/why的程序测试/AI_Lab/AI-Avatar-Creater/hackweek/app/crawl/static/out'
-<<<<<<< HEAD
-OUT_PATH = '/crawl/static/out'
-=======
 basedir = os.path.dirname(__file__)
 OUT_PATH = basedir + '/static/out'
->>>>>>> 2d54ab7cd975a10a4efe9b64e8d6dae19d934f57
 
 @crawl.route('/',methods=['GET'])
 def crawl_main():
