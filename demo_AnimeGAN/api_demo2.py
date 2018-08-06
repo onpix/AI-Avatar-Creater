2e710ebf9d928cd4fa35f21500da5a9e4afee3b0
creeper121386 <694029828@qq.com>
2018-08-06 19:07:38 +0800

    add hd pic

--- demo_AnimeGAN/api_demo2.py
+++ demo_AnimeGAN/api_demo2.py
@@ -4,7 +4,7 @@ import torch
 import torchvision
 cuda = True
 device = torch.device(
-    'cuda') if torch.cuda.is_available() and opt.cuda else torch.device('cpu')
+    'cuda') if torch.cuda.is_available() and cuda else torch.device('cpu')
 nz = 100
 
 #########################    
@@ -14,10 +14,10 @@ nz = 100
 # batch_size = 1       #how many figures in a output image.')
 # modelNum = [x*10+1 for x in range(10)]+[100]
 
-batchSize = opt.batch_size
 workDir = os.getcwd()
-savePath = workDir + '/output'
 modelPath = workDir + '/model_normal'
+# savePath = workDir + '/output'
+# batchSize = opt.batch_size
 #################################################
 
 class TestNet(object):
@@ -31,7 +31,7 @@ class TestNet(object):
 ################ init noise code z: #################
 
     def init(self):
-        if opt.tune:
+        if self.tune:
             self.img_num = 1
             tmp = torch.randn(nz, 1, 1)
             initZ = torch.randn(batchSize, nz, 1, 1)
@@ -46,7 +46,7 @@ class TestNet(object):
             GmodelPath = modelPath + '/Gnn-epoch{}.pkl'.format(n)
             self.G.load_state_dict(torch.load(GmodelPath))
             for i in range(self.img_num):
-                if opt.tune:
+                if self.tune:
                     z = self.initZ
                 else:
                     z = torch.randn(batchSize, nz, 1, 1).to(device)
@@ -58,7 +58,7 @@ class TestNet(object):
 #Dnn = D()
 # Dnn.load_state_dict(torch.load(modelPath))
 
-def main(mode=0, model_num=0, img_num=4, batch_size=1, tune=False):
+def main(mode=0, model_num=0, img_num=4, batch_size=1, tune=False, out_path):
     modelNum = [41, 61, 81, '20_1'] if mode==0 else [31, 71, '_origin']
     modelNum = [modelNum[model_num], ]
     testNet = TestNet(mode, modelNum, img_num, batch_size, tune)
@@ -66,4 +66,4 @@ def main(mode=0, model_num=0, img_num=4, batch_size=1, tune=False):
     testNet.test()
 
 if __name__ == '__main__':
-    main(mode=0, model_num=0, img_num=4, tune=False)
+    main(mode=0, model_num=0, img_num=4, tune=False, out_path='./output')
